e3dbc54a4331ef61b8f7c6431e58638bf57b6dbb
fleetingold <1161851523@qq.com>
2020-10-23 18:19:14 +0800

    Carding ICBC API NOW

--- src/com/mystudy/icbc/request/ConstTransCode.java
+++ src/com/mystudy/icbc/request/ConstTransCode.java
@@ -12,19 +12,29 @@ public class ConstTransCode {
 	public static final String QUERY_BALANCEDETAIL = "QHISD";
 	
 	/**
-	 * 	对账单
+	 * 	查询当日明细
 	 */
-	public static final String QUERY_QBILL = "QBILL";
-	
-	/**
-	 * 	批量电子回单指令查询
-	 */
-	public static final String QUERY_QEBILL = "QEBILL";
+	public static final String QUERY_QPD = "QPD";
 	
 	/**
 	 * 	电子回单批量查询
 	 */
 	public static final String QUERY_BATEBILL = "BATEBILL";
+	
+	/**
+	 * 	电子回单批量查询-包指令查询
+	 */
+	public static final String QUERY_QEBILL = "QEBILL";
+	
+	/**
+	 * 	电子回单下载
+	 */
+	public static final String QUERY_DownEBill = "DownEBill";
+	
+	/**
+	 * 	对账单
+	 */
+	public static final String QUERY_QBILL = "QBILL";
 	
 	/**
 	 * 	网点信息下载
@@ -35,14 +45,4 @@ public class ConstTransCode {
 	 * 	行别行号下载
 	 */
 	public static final String QUERY_DIBPSBC = "DIBPSBC";
-	
-	/**
-	 * 	查询当日明细
-	 */
-	public static final String QUERY_QPD = "QPD";
-	
-	/**
-	 * 	电子回单下载
-	 */
-	public static final String QUERY_DownEBill = "DownEBill";
 }
