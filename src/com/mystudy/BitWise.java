f498fa9a31f75fc85bb7239ea1e750f39a2db295
fleetingold <1161851523@qq.com>
2020-10-26 09:45:55 +0800

    Review And Add Comments

--- src/com/mystudy/BitWise.java
+++ src/com/mystudy/BitWise.java
@@ -1,5 +1,10 @@
 package com.mystudy;
 
+/**
+ * @author hy1
+ *
+ *	位运算:与、或、异或、非、左移、右移
+ */
 public class BitWise {
 
 	public static void main(String[] args) {
@@ -7,24 +12,32 @@ public class BitWise {
 		int a = 60; /* 60 = 0011 1100 */
 		int b = 13; /* 13 = 0000 1101 */
 		int c = 0;
+		
+		// 与
 		c = a & b;	/* 12 = 0000 1100 */
 		System.out.println("a & b = " + c);
 		
+		// 或
 		c = a | b;	/* 61 = 0011 1101 */
 		System.out.println("a | b = " + c);
 		
+		// 异或
 		c = a ^ b;	/* 49 = 0011 0001 */
 		System.out.println("a ^ b = " + c);
 		
+		// 非
 		c = ~a;	/* -61 = 1100 0011 */
 		System.out.println("~a = " + c);
 		
+		// 左移
 		c = a << 2;	/* 240 = 1111 0000 */
 		System.out.println("a << b = " + c);
 		
+		// 右移
 		c = a >> 2;	/* 15 = 1111 */
 		System.out.println("a >> 2 = " + c);
 		
+		// 右移
 		c = a >>> 2;	/* 15 = 0000 1111 */
 		System.out.println("a >>> 2 = " + c);
 	}
--- src/com/mystudy/Hello.java
+++ src/com/mystudy/Hello.java
@@ -5,9 +5,24 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int i = 1;
-		if (i <= 0)
+		if (i <= 0) {
 			System.out.println("这里不会被执行!!!");
-		else
+		} else {
+			// 原始字符 'a' 装箱到 Character 对象 ch 中
+			Character ch = 'a';
+			System.out.println("ch = " + ch);
+			
+			// Unicode 字符表示形式
+			char uniChar = '\u039A';
+			System.out.println("uniChar = " + uniChar);
+			
+			byte c = 0x4a; /* c = 74 */
+			System.out.println("c = " + c);
+			
+			char[] helloArray = { 'r', 'u', 'n', 'o', 'o', 'b' };
+			String helloString = new String(helloArray);
+			System.out.println(helloString);
 			System.out.println("Hello Java World!!!");
+		}
 	}
 }
