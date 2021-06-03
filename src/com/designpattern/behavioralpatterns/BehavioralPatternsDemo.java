a7bfd3efc1149f3fa2489f872f5656ae6e9e147f
fleetingold <1161851523@qq.com>
2021-06-03 16:28:41 +0800

    Design Pattern -> Behavioral Patterns -> ChainPatternDemo

---
+++ src/com/designpattern/behavioralpatterns/BehavioralPatternsDemo.java
@@ -0,0 +1,9 @@
+package com.designpattern.behavioralpatterns;
+
+public class BehavioralPatternsDemo {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("行为型模式 [Behavioral Patterns]");
+		System.out.println("行为型模式 这些设计模式特别关注对象之间的通信。");
+	}
+}
