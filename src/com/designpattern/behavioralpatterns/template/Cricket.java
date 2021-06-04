173b31e287c2f9a1f41ed3a00ebe21bfd1113b4e
fleetingold <1161851523@qq.com>
2021-06-04 17:29:45 +0800

    Design Pattern -> Behavioral Patterns -> TemplatePatternDemo

---
+++ src/com/designpattern/behavioralpatterns/template/Cricket.java
@@ -0,0 +1,22 @@
+package com.designpattern.behavioralpatterns.template;
+
+public class Cricket extends Game {
+
+	@Override
+	void initialize() {
+		// TODO Auto-generated method stub
+		System.out.println("Cricket Game Initialized! Start playing.");
+	}
+
+	@Override
+	void startPlay() {
+		// TODO Auto-generated method stub
+		System.out.println("Cricket Game Startd. Enjoy the game!");
+	}
+
+	@Override
+	void endPlay() {
+		// TODO Auto-generated method stub
+		System.out.println("Cricket Game Finished!");
+	}
+}
---
+++ src/com/designpattern/behavioralpatterns/template/Football.java
@@ -0,0 +1,22 @@
+package com.designpattern.behavioralpatterns.template;
+
+public class Football extends Game {
+
+	@Override
+	void initialize() {
+		// TODO Auto-generated method stub
+		System.out.println("Football Game Initialized! Start playing.");
+	}
+
+	@Override
+	void startPlay() {
+		// TODO Auto-generated method stub
+		System.out.println("Football Game Started. Enjoy the game!");
+	}
+
+	@Override
+	void endPlay() {
+		// TODO Auto-generated method stub
+		System.out.println("Football Game Finished!");
+	}
+}
